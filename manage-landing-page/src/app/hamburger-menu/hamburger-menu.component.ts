5e34a6ce05e8dfd21c1f5b11f8478e83b82dd2ce
ShadowBlack <daniel.miliczek@op.pl>
2024-02-04 16:34:51 +0100

    ham menu

---
+++ manage-landing-page/src/app/hamburger-menu/hamburger-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-hamburger-menu',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './hamburger-menu.component.html',
+})
+export class HamburgerMenuComponent {
+
+}
--- manage-landing-page/src/app/header/header.component.ts
+++ manage-landing-page/src/app/header/header.component.ts
@@ -1,18 +1,23 @@
 import { Component, ElementRef, ViewChild, HostListener} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { HamburgerMenuComponent } from '../hamburger-menu/hamburger-menu.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    HamburgerMenuComponent
   ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   @ViewChild('hamButton') hamButton!: ElementRef;
+  @ViewChild('hamMenu', { read: ElementRef }) hamMenu!: ElementRef;
 
   hamMenuClick() {
     this.hamButton.nativeElement.classList.toggle('showed-ham-menu');
+    this.hamMenu.nativeElement.classList.toggle('ham-menu');
   }
 }
